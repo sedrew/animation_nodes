314ef32d8c81cef2a29943f7fc7d15f5fd2dfc59
3DSingh <kadarwalaks@gmail.com>
2019-12-08 15:00:47 +0530

    Improved the code particles output, set bevel edge/vertex weights and uv map data input/output nodes.

--- animation_nodes/nodes/newnodes/uv_map_data_input.py
+++ animation_nodes/nodes/newnodes/uv_map_data_input.py
@@ -1,8 +1,9 @@
 import bpy
+import numpy as np
 from bpy.props import *
 from ... events import propertyChanged
-from ... data_structures import DoubleList
 from ... base_types import AnimationNode
+from ... data_structures import DoubleList, VirtualDoubleList
 
 mapIdentifierTypeItems = [
     ("INDEX", "Index", "Get uv map based on the index", "NONE", 0),
@@ -12,6 +13,7 @@ mapIdentifierTypeItems = [
 class UVMapDataInputNode(bpy.types.Node, AnimationNode):
     bl_idname = "an_UVMapDataInputNode"
     bl_label = "UV Map Data Input"
+    errorHandlingType = "EXCEPTION"
 
     mapIdentifierType = EnumProperty(name = "UV Map Identifier Type", default = "INDEX",
         items = mapIdentifierTypeItems, update = AnimationNode.refresh)
@@ -23,27 +25,31 @@ class UVMapDataInputNode(bpy.types.Node, AnimationNode):
         elif self.mapIdentifierType == "NAME":
             self.newInput("Text", "Name", "uvMapName")
 
-        self.newOutput("Float List", "x", "x")
-        self.newOutput("Float List", "y", "y")
+        self.newOutput("Float List", "X", "x")
+        self.newOutput("Float List", "Y", "y")
 
     def drawAdvanced(self, layout):
         layout.prop(self, "mapIdentifierType", text = "Type")
 
     def execute(self, object, identifier):
-        x = []
-        y = []
         if object is None:
-            return DoubleList.fromValues(x), DoubleList.fromValues(y)
+            return DoubleList(), DoubleList()
+
         uvMap = self.getUVMap(object, identifier)
-        if uvMap is None:
-            return DoubleList.fromValues(x), DoubleList.fromValues(y)
-        coList= [0., 0.]*len(uvMap.data)
+        
+        x = VirtualDoubleList.create(0, 0)
+        y = VirtualDoubleList.create(0, 0)
+
+        coList = np.zeros((2 * len(uvMap.data)), dtype=float)
         uvMap.data.foreach_get('uv', coList)
-        end = len(coList) 
-        x = coList[:end:2]
-        y = coList[1:end:2]
+        x = coList[::2]
+        y = coList[1::2]
         return DoubleList.fromValues(x), DoubleList.fromValues(y)
 
     def getUVMap(self, object, identifier):
+        if object.type != "MESH": 
+            self.raiseErrorMessage("No mesh object.")
+
         try: return object.data.uv_layers[identifier]
-        except: return None
+        except: self.raiseErrorMessage("UV Map is not found.")
+
